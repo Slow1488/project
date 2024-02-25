12619272522399203dd4cba49802f49ee3315dcc
Slow1488 <159696752+Slow1488@users.noreply.github.com>
2024-02-25 15:13:01 +0300

    Add files via upload

---
+++ example.py
@@ -0,0 +1,30 @@
+from utils import save_images
+from yandex_images_parser import Parser
+
+parser = Parser()
+
+# Find a square, medium-sized image of a cat:
+square_cat = parser.query_search(query="cat",
+                                 limit=1,
+                                 size=parser.size.medium,
+                                 orientation=parser.orientation.square)
+
+# Since the query_search method returns a list, we select the first element:
+square_cat_url = square_cat[0]
+
+# Find images of similar cats:
+similar_cats = parser.image_search(url=square_cat_url,
+                                   limit=10)
+
+# Display the results:
+print(f"Cat:\n{square_cat_url}\n")
+
+print("Similar cats:")
+for cat in similar_cats:
+    print(cat)
+
+# Save the images:
+output_dir = "./images/cat"
+
+save_images(square_cat, dir_path=output_dir, prefix="original_")  # Add prefix to filenames
+save_images(similar_cats, dir_path=output_dir, prefix="similar_", number_images=True)  # Add prefix and numbering
